f7e165669595e2f7a60bb26acdf54dd96adca9c8
Thiago Alves <thiagoa@facts-eng.com>
2025-10-16 13:42:34 -0700

    small fixes

--- core/src/plc_app/debug_handler.c
+++ core/src/plc_app/debug_handler.c
@@ -5,6 +5,7 @@ size_t process_debug_data(uint8_t *data, size_t length)
 {
     // Process the debug data as needed
     // This is a placeholder implementation
+    data[0] = 0;
     log_info("Processing debug data of length %zu", length);
     return length;
 }
