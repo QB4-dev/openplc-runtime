63b188638eff9c94d27f111ddb0eef3b7a5f6b82
Marcone Tenorio <marconetsf@gmail.com>
2025-12-09 09:54:23 +0100

    Enhance plugin stop and restart functions to skip disabled plugins

--- core/src/drivers/plugin_driver.c
+++ core/src/drivers/plugin_driver.c
@@ -399,14 +399,21 @@ int plugin_driver_stop(plugin_driver_t *driver)
     // Signal all plugins to stop
     for (int i = 0; i < driver->plugin_count; i++)
     {
+        plugin_instance_t *plugin = &driver->plugins[i];
+        
+        // Skip disabled plugins
+        if (!plugin->config.enabled)
+        {
+            printf("[PLUGIN]: Skipping disabled plugin during stop: %s\n", plugin->config.name);
+            continue;
+        }
+        
         printf("[PLUGIN]: Stopping plugin %d/%d: %s\n", i + 1, driver->plugin_count,
                driver->plugins[i].config.name);
-        if (driver->plugins[i].python_plugin && driver->plugins[i].python_plugin->pFuncStop &&
-            driver->plugins[i].running)
+        if (plugin->python_plugin && plugin->python_plugin->pFuncStop &&
+            plugin->running)
         {
-            plugin_instance_t *plugin = &driver->plugins[i];
-
-            PyObject *res = PyObject_CallNoArgs(driver->plugins[i].python_plugin->pFuncStop);
+            PyObject *res = PyObject_CallNoArgs(plugin->python_plugin->pFuncStop);
             if (!res)
             {
                 PyErr_Print();
@@ -421,16 +428,15 @@ int plugin_driver_stop(plugin_driver_t *driver)
             plugin->running = 0;
         }
 
-        else if (driver->plugins[i].native_plugin && driver->plugins[i].native_plugin->stop &&
-                 driver->plugins[i].running)
+        else if (plugin->native_plugin && plugin->native_plugin->stop &&
+                 plugin->running)
         {
-            plugin_instance_t *plugin = &driver->plugins[i];
             plugin->native_plugin->stop();
             printf("[PLUGIN]: Native plugin %s stopped successfully.\n", plugin->config.name);
             plugin->running = 0;
         }
 
-        printf("[PLUGIN]: Plugin %s stopped...\n", driver->plugins[i].config.name);
+        printf("[PLUGIN]: Plugin %s stopped...\n", plugin->config.name);
         // Plugin manager only handles destruction, not stopping
     }
 
@@ -458,6 +464,14 @@ int plugin_driver_restart(plugin_driver_t *driver)
     for (int i = 0; i < driver->plugin_count; i++)
     {
         plugin_instance_t *plugin = &driver->plugins[i];
+        
+        // Skip disabled plugins during cleanup
+        if (!plugin->config.enabled)
+        {
+            printf("[PLUGIN]: Skipping disabled plugin during restart cleanup: %s\n", plugin->config.name);
+            continue;
+        }
+        
         if (plugin->python_plugin)
         {
             python_plugin_cleanup(plugin);
@@ -957,9 +971,8 @@ void python_plugin_cycle(plugin_instance_t *plugin)
 // Cleanup Python plugin
 static void python_plugin_cleanup(plugin_instance_t *plugin)
 {
-    (void)plugin; // Suppress unused parameter warning
     // Cleanup Python resources
-    if (plugin && plugin->python_plugin)
+    if (plugin && plugin->python_plugin && plugin->config.enabled)
     {
         // Call cleanup function if available
         if (plugin->python_plugin->pFuncCleanup)
